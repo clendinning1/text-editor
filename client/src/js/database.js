24a55b0a1020ae03d069baf6ae7a460f863de9ba
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-05-05 16:53:12 -0500

    put req for database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,33 +13,27 @@ const initdb = async () =>
     },
   });
 
-
-
   
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.log('Put to the database');
 
-// example
-// // Export a function we will use to POST to the database.
-// export const postDb = async (content) => {
-//   console.log('Post to the database');
+  // connect to db
+  const jateDb = await openDB('jate', 1);
 
-//   // Create a connection to the database database and version we want to use.
-//   const todosDb = await openDB('todos', 1);
+  // create new transaction ('[database]', '[data privileges]')
+  const tx = jateDb.transaction('jate', 'readwrite');
 
-//   // Create a new transaction and specify the database and data privileges.
-//   const tx = todosDb.transaction('todos', 'readwrite');
+  // open the object store
+  const store = tx.objectStore('jate');
 
-//   // Open up the desired object store.
-//   const store = tx.objectStore('todos');
+  // add method passes content into the store
+  const request = store.add({ jate: content });
 
-//   // Use the .add() method on the store and pass in the content.
-//   const request = store.add({ todo: content });
-
-//   // Get confirmation of the request.
-//   const result = await request;
-//   console.log('Data saved to the database', result);
-// };
+  // req confirmation
+  const result = await request;
+  console.log('Data saved to the database', result);
+}
 
 
 
