9ad6bdb85283311a277921666e46606dc5715a00
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-05-07 20:37:15 -0500

    installer working

--- client/src-sw.js
+++ client/src-sw.js
@@ -67,4 +67,25 @@ registerRoute(
 
 
 
+// does this go here??????????????????????????????????
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('./service-worker.js').then(function (registration) {
+      // registration successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function (err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+this.addEventListener('fetch', function (event) {
+  // intentionally empty fetch function is required for the SW to be detected
+});
+
+
+
+
 registerRoute();
